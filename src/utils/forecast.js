4c867064ddfeb0cff7b7c6105d58cf817d929dfb
Divisha Bapna <bapnadivisha@gmail.com>
2020-07-02 02:34:22 +0530

    Add the humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,7 @@ const forecast=(longitude,latitude,callback)=>
         else if(body.error)
         callback('Unable to find location. Try another search',undefined)
         else
-        callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degree out. It feels like ' + body.current.feelslike+' degree out.'
-            )
+        callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degree. It feels like ' + body.current.feelslike+' degree out. Humidity is ' + body.current.humidity +'% .')
     }
 
     )
